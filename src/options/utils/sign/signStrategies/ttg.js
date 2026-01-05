b8d3030e2e62ea3312a74c22fe1a9043b03dc01c
蓝芷怡 <kaze.liu@qq.com>
2026-01-05 17:24:06 +0800

    :bug:  修复签到功能选择器和状态判断逻辑

--- src/options/utils/sign/signStrategies/ttg.js
+++ src/options/utils/sign/signStrategies/ttg.js
@@ -1,13 +1,5 @@
 export function ttgMain() {
-    let signedHtml = document.querySelector('.smallfont #sp_signed a#signed');
-    if (signedHtml?.innerText === '签到') {
-        signedHtml.click();
-        return {
-            sign: true,
-            title: '签到成功',
-            text: signedHtml.innerText
-        }
-    }
+    let signedHtml = document.querySelector('.smallfont #sp_signed');
     if (signedHtml?.innerText === '已签到') {
         return {
             sign: true,
@@ -15,6 +7,17 @@ export function ttgMain() {
             text: signedHtml.innerText
         }
     }
+
+    let signBtnHtml = document.querySelector('.smallfont #sp_signed a#signed');
+    if (signBtnHtml?.innerText === '签到') {
+        signBtnHtml.click();
+        return {
+            sign: true,
+            title: '签到成功',
+            text: signBtnHtml.innerText
+        }
+    }
+
     return {
         sign: false
     };
